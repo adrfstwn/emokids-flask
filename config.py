45cd3b85855dab8f9ad34a2f494ba39992e2bc2f
adrfstwn <100577989+adrfstwn@users.noreply.github.com>
2024-06-18 21:12:06 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://postgres:root@127.0.0.1:5432/emokids-flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
