8b16cd151da30287661f79dd2e6f0ea11a60245f
adrfstwn <100577989+adrfstwn@users.noreply.github.com>
2024-07-10 20:38:21 +0700

    ganti sql

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:root@127.0.0.1:5432/emokids-flask'
+        'mysql://root:@127.0.0.1:3306/emokids'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ASSETS_ROOT = os.environ.get('ASSETS_ROOT') or '/static/assets'
     DEBUG = True
