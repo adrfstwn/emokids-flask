088eabcf5042dd526c105877902f723be918b453
adrfstwn <100577989+adrfstwn@users.noreply.github.com>
2024-07-24 22:16:05 +0700

    w

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    FLASK_APP = 'app.py'
-    SECRET_KEY = 'aGVuU2VjdXJlX3NlY3JldF9rZXktQWxvbmdTdHJpbmdUZXh0'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1:3306/emokids'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ASSETS_ROOT = 'static/assets'
-    FLASK_ENV = 'development'
